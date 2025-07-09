c78c52df41ec4568d1df4e5d3fae754a728f9197
Jeff <salisbury.jeffery@gmail.com>
2025-07-08 22:30:42 -0400

    Adding minor functions to convert to american...

--- utils.py
+++ utils.py
@@ -14,3 +14,18 @@ class ForecastResult:
 def temp_to_fahrenheit(temp_c: float) -> float:
     """Convert Celsius to Fahrenheit."""
     return (temp_c * 9/5) + 32
+
+
+def kph_to_mph(speed_kph: float) -> float:
+    """Convert kilometers per hour to miles per hour."""
+    return speed_kph * 0.621371
+
+
+def military_to_standard(hour: int) -> int:
+    """Convert 24-hour military time to 12-hour standard time."""
+    if hour == 0:
+        return 12  # Midnight
+    elif hour > 12:
+        return hour - 12
+    else:
+        return hour
